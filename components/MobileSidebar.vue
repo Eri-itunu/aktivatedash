91b3acbb54b834da48a02dac38f2e5cc6fa5b981
eriitunu-a <eritunu.adesioye@useaktivate.com>
2024-04-14 19:43:30 +0100

    Update MobileSidebar.vue

--- components/MobileSidebar.vue
+++ components/MobileSidebar.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="flex px-2 h-full bg-[#4A5568] bg-opacity-50 w-screen">
-        <div class="bg-[#090618] overflow-y-scroll h-screen flex flex-col gap-5 relative justify-between  rounded-[20px]" >
+    <div class="flex px-2 h-screen bg-[#4A5568] bg-opacity-50 w-screen">
+        <div class="bg-[#090618] overflow-y-scroll  flex flex-col gap-5 relative justify-between  rounded-[20px]" >
             <div class="px-16 pt-12">
                 <img src="/assets/icons/AktivateLogo.svg" class="" alt="">
 
