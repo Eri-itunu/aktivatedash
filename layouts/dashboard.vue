0fd69f414dbeb02397cd5ef53f2935545352571c
eriitunu-a <eritunu.adesioye@useaktivate.com>
2024-04-14 19:55:24 +0100

    Update dashboard.vue

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -3,11 +3,11 @@
     <div class="flex px-2 h-screen w-screen bg-[#100C21]">
         <div class="absolute  z-50 w-screen flex h-screen items-center md:hidden duration-700 ease-in-out" 
         :class="{ 'left-[-100%] ': !sidebar, 'left-[0]': sidebar }">
-            <MobileSidebar />
+            <MobileSidebar :toggleSidebar = "toggleSidebar" />
         </div>
         
         <div class= " hidden py-5  md:block md:w-96 ">
-            <Sidebar :toggleSidebar = "toggleSidebar" />
+            <Sidebar  />
         </div>
 
         <div class="w-full overflow-hidden py-5 overflow-y-scroll h-screen ">
