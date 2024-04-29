5d3cf8f56bfd8020e7b4a872a2c1477536cc8017
eiromz <eronmonsele.adeyemo@gmail.com>
2024-04-29 22:39:22 +0100

    pushing updates

--- pages/creator/dashboard/platforms.vue
+++ pages/creator/dashboard/platforms.vue
@@ -1,8 +1,4 @@
 <script setup lang="ts">
-// For Nuxt Bridge
-
-// For Nuxt 3
-import {runtime} from "std-env";
 
 definePageMeta({
   layout: 'dashboard',
@@ -13,20 +9,16 @@ const isOpen = ref(false)
 
 const apiUrl = useRuntimeConfig().public.API_URL
 
-const facebook_instagram_url =  `${apiUrl}/facebook/login`
+const facebook_instagram_url =  `${apiUrl}/platform/facebook/login`
 
 </script>
 
-
 <template>
   <div class="flex justify-end mt-5 items-end mb-10">
-
     <button label="Open" @click="isOpen = true" class="bg-[#5331E8] text-white rounded-[100px] px-4 py-2 ">
       Link Social Media Accounts
     </button>
-
   </div>
-
 
   <UModal v-model="isOpen" prevent-close>
     <UCard :ui="{ ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
@@ -38,28 +30,25 @@ const facebook_instagram_url =  `${apiUrl}/facebook/login`
           <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1" @click="isOpen = false"/>
         </div>
       </template>
-
       <div class="flex flex-col gap-2 ">
         <h4>Link Manually</h4>
         <p>To link social media platforms and retrieve key metrics click on your app of choice, fill in your details and start getting feedback!</p>
         <div class="flex mt-4 gap-2 items-center">
 
-
           <NuxtLink :to=facebook_instagram_url target="_blank">
-            <img src="../../../assets/icons/facebook.svg" alt="">
+            <img src="~assets/icons/facebook.svg" alt="">
           </NuxtLink>
-
           <div class="w-20 h-px bg-[#464160]"></div>
           <NuxtLink :to=facebook_instagram_url target="_blank">
-            <img src="../../../assets/icons/Insta.svg" alt="">
+            <img src="~assets/icons/Insta.svg" alt="">
           </NuxtLink>
           <div class="w-20 h-px bg-[#464160]"></div>
           <button>
-            <img src="../../../assets/icons/snapchat.svg" alt="">
+            <img src="~assets/icons/snapchat.svg" alt="">
           </button>
           <div class="w-20 h-px bg-[#464160]"></div>
           <button>
-            <img src="../../../assets/icons/tiktok.svg" alt="">
+            <img src="~assets/icons/tiktok.svg" alt="">
           </button>
         </div>
       </div>
@@ -71,7 +60,6 @@ const facebook_instagram_url =  `${apiUrl}/facebook/login`
       <div class="flex items-center justify-center py-8 w-1/12 border-r-2 border-darkBlue ">
         <img src="../../../assets/icons/Insta.svg" class="h-10 w-auto" alt="">
       </div>
-
       <div class="w-11/12 flex  ">
         <div class="w-1/3 border-r-2 py-4 border-darkBlue">
           <div class="flex justify-between gap-2 items-center px-4">
@@ -79,7 +67,6 @@ const facebook_instagram_url =  `${apiUrl}/facebook/login`
               <img src="~/assets/icons/aktivate-logo-small.svg" alt="">
               <div> Followers</div>
             </div>
-
           </div>
           <!-- End Top part -->
 
@@ -164,9 +151,7 @@ const facebook_instagram_url =  `${apiUrl}/facebook/login`
         </div>
 
       </div>
-
     </div>
-
     <div class="flex flex-row rounded-lg bg-vDarkBlue text-white ">
       <div class="flex items-center justify-center py-8 w-1/12 border-r-2 border-darkBlue ">
         <img src="../../../assets/icons/facebook.svg" class="h-10 w-auto" alt="">
