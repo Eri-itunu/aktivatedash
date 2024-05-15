86cc0e348d479465870a61634aa61fb7fea96543
akinola <akin.akinleye619@gmail.com>
2024-05-15 14:44:04 +0100

    spinner bug-fix

--- components/Spinner.vue
+++ components/Spinner.vue
@@ -1,11 +1,13 @@
-
 <script setup lang="ts">
-  defineProps({
-    loading: Boolean
-  })
-
+defineProps({
+  loading: Boolean,
+});
 </script>
 
 <template>
-  <div v-if="loading" class="p-3 border-2 border-grey2 border-t-black rounded-full w-min text-center animate-spin"> </div> <!-- spinner -->
+  <div
+    v-if="loading"
+    class="p-3 border-2 border-grey2 border-t-black rounded-full w-min h-min text-center animate-spin"
+  ></div>
+  <!-- spinner -->
 </template>
