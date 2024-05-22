997c9fcae8cf1d61e355d90d7c25394cdfa54e14
akinola <akin.akinleye619@gmail.com>
2024-05-22 20:18:59 +0100

    failed trials

---
+++ utils/index.ts
@@ -0,0 +1,13 @@
+export const promisify = (fn: any) => {
+  return function (...args) {
+    return new Promise((resolve, reject) => {
+      fn(...args, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  };
+}
