e4ad733ee51337b7b5beb525750dfc2a500ab9f0
akinola <akin.akinleye619@gmail.com>
2024-05-24 14:29:34 +0100

    creator card update

--- components/brands/CampaignCardM.vue
+++ components/brands/CampaignCardM.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
+import type { ICampaign } from "types";
+import { calcProgress } from "../../utils";
+const props = defineProps<{ campaign: ICampaign }>();
 
-  import type { ICampaign } from "types";
-  const props = defineProps<{ campaign: ICampaign}>()
-
-
+const progress = calcProgress(props.campaign.start_date, props.campaign.end_date);
 </script>
 
 <template>
-  <div @click="$router.push(`/brands/dashboard/campaigns/${campaign.id}`)"  class="min-w-[27rem] cursor-pointer md:max-w-[520px]  rounded-lg bg-vDarkBlue text-white pb-2">
+  <div
+    @click="$router.push(`/brands/dashboard/campaigns/${campaign.id}`)"
+    class="min-w-[27rem] cursor-pointer md:max-w-[520px] rounded-lg bg-vDarkBlue text-white pb-2"
+  >
     <!--  Section A-->
-    <div class="px-3  border-b border-b-darkBlue">
+    <div class="px-3 border-b border-b-darkBlue">
       <div class="flex justify-between py-3 items-end">
-        
         <!-- <ul class="flex flex-row justify-end list-none">
           <li class="mr-[-1em] z-[1]">
             <img class="rounded-[50%] border-2 border-[background: #464160]" src="../../assets/images/Avatar4.png" alt="">
@@ -27,14 +29,13 @@
         <!-- <p class="underline text-grey1 text-xs"> view report</p> -->
       </div>
       <!-- image part -->
-      <p class=" line-clamp-2 text-ellipsis mb-2">{{ campaign.headline }}</p>
-      <div class="flex relative justify-center  bg-purplelabel rounded-lg">
-        <img src="/assets/images/created.svg" class="" alt="">
+      <p class="line-clamp-2 text-ellipsis mb-2">{{ campaign.headline }}</p>
+      <div class="flex relative justify-center bg-purplelabel rounded-lg">
+        <img src="/assets/images/created.svg" class="" alt="" />
       </div>
       <!-- end image part -->
-      <p class="uppercase text-xs font-thin text-grey2 my-2"> Campaign Description</p>
-      <p class=" line-clamp-2 text-ellipsis mb-2">{{ campaign.description }}</p>
-      
+      <p class="uppercase text-xs font-thin text-grey2 my-2">Campaign Description</p>
+      <p class="line-clamp-2 text-ellipsis mb-2">{{ campaign.description }}</p>
     </div>
     <!-- end Section A -->
 
@@ -52,31 +53,80 @@
             </div>
             <!-- end icon thing-->
             <div class="text-sm text-[#CDC2FF] text-nowrap">
-              <p> Start Date: <span class="font-light text-xs pl-3.5">{{campaign.start_date.split("T")[0]}}</span></p>
-              <p> End Date: <span class="font-light text-xs pl-5">{{campaign.end_date.split("T")[0]}}</span></p>
+              <p>
+                Start Date:
+                <span class="font-light text-xs pl-3.5">{{
+                  campaign.start_date.split("T")[0]
+                }}</span>
+              </p>
+              <p>
+                End Date:
+                <span class="font-light text-xs pl-5">{{
+                  campaign.end_date.split("T")[0]
+                }}</span>
+              </p>
             </div>
           </div>
           <!-- End Date part  -->
 
           <!-- icons part -->
           <div class="flex gap-1 overflow-hidden">
-            <img v-if="campaign?.deliverables?.platform.includes('facebook')" class="object-contain" src="/assets/icons/collab/facebook.svg" alt="">
-            <img v-if="campaign?.deliverables?.platform.includes('instagram')" class="object-contain" src="/assets/icons/collab/instagram.svg" alt="">
-            <img v-if="campaign?.deliverables?.platform.includes('tiktok')" class="object-contain" src="/assets/icons/collab/tiktok.svg" alt="">
-            <img v-if="campaign?.deliverables?.platform.includes('x')" class="object-contain" src="/assets/icons/collab/twitter.svg" alt="">
-            <img v-if="campaign?.deliverables?.platform.includes('whatsapp')" class="object-contain" src="/assets/icons/collab/whatsapp.svg" alt="">
-            <img v-if="campaign?.deliverables?.platform.includes('snapchat')" class="object-contain" src="/assets/icons/collab/snapchat.svg" alt="">
-            <img v-if="campaign?.deliverables?.platform.includes('linkedin')" class="object-contain" src="/assets/icons/collab/linkedin.svg" alt="">
-            <img v-if="campaign?.deliverables?.platform.includes('youtube')" class="object-contain" src="/assets/icons/collab/youtube.svg" alt="">
+            <img
+              v-if="campaign?.deliverables?.platform.includes('facebook')"
+              class="object-contain"
+              src="/assets/icons/collab/facebook.svg"
+              alt=""
+            />
+            <img
+              v-if="campaign?.deliverables?.platform.includes('instagram')"
+              class="object-contain"
+              src="/assets/icons/collab/instagram.svg"
+              alt=""
+            />
+            <img
+              v-if="campaign?.deliverables?.platform.includes('tiktok')"
+              class="object-contain"
+              src="/assets/icons/collab/tiktok.svg"
+              alt=""
+            />
+            <img
+              v-if="campaign?.deliverables?.platform.includes('x')"
+              class="object-contain"
+              src="/assets/icons/collab/twitter.svg"
+              alt=""
+            />
+            <img
+              v-if="campaign?.deliverables?.platform.includes('whatsapp')"
+              class="object-contain"
+              src="/assets/icons/collab/whatsapp.svg"
+              alt=""
+            />
+            <img
+              v-if="campaign?.deliverables?.platform.includes('snapchat')"
+              class="object-contain"
+              src="/assets/icons/collab/snapchat.svg"
+              alt=""
+            />
+            <img
+              v-if="campaign?.deliverables?.platform.includes('linkedin')"
+              class="object-contain"
+              src="/assets/icons/collab/linkedin.svg"
+              alt=""
+            />
+            <img
+              v-if="campaign?.deliverables?.platform.includes('youtube')"
+              class="object-contain"
+              src="/assets/icons/collab/youtube.svg"
+              alt=""
+            />
           </div>
           <!-- end icons part -->
         </div>
       </div>
- 
-      <!-- progress bar -->
-      <ProgressBar :percentage="80" />
-      <!-- end progress bar -->
 
+      <!-- progress bar -->
+      <ProgressBar :percentage="progress" />
+      <!-- end progress bar -->
     </div>
     <!--  end Section B-->
 
@@ -95,7 +145,9 @@
         <p class="uppercase font-bold text-xs md:text-lg">20%</p>
       </div>
       <div class="flex flex-col">
-        <p class="uppercase font-thin text-xs break-word  text-grey2">top age demographic</p>
+        <p class="uppercase font-thin text-xs break-word text-grey2">
+          top age demographic
+        </p>
         <p class="uppercase font-bold text-xs md:text-lg">21 - 34 (54% F)</p>
       </div>
       <div class="flex flex-col">
@@ -108,7 +160,5 @@
       </div>
     </div>
     <!--  -->
-
-
   </div>
 </template>
