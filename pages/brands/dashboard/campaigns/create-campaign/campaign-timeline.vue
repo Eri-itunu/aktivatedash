243c62b3468036dc7179353f2d83e016f347ebd6
akinola <akin.akinleye619@gmail.com>
2024-06-07 01:04:40 +0100

    timeline update

--- pages/brands/dashboard/campaigns/create-campaign/campaign-timeline.vue
+++ pages/brands/dashboard/campaigns/create-campaign/campaign-timeline.vue
@@ -1,99 +1,93 @@
-<script setup lang="ts" >
-import { ref } from 'vue';
-
+<script setup lang="ts">
+import { ref } from "vue";
 
 definePageMeta({
-layout: 'brands',
-})
+  layout: "brands",
+});
 
-import { format } from 'date-fns'
-
+import { format } from "date-fns";
 
 const createBrandCampaignStore = useCreateBrandCampaignStore();
 
 const { startDate, endDate, submissionDueDate } = storeToRefs(createBrandCampaignStore);
-
 </script>
 
 <template>
-    <div class="px-2 md:px-12">
-        <brandsCampaignStage v-bind:content = "true" v-bind:influencer = "true"/>
-        <div class="bg-vDarkBlue text-white flex items-center mt-5 flex-col gap-5 p-16">
+  <div class="px-2 md:px-12">
+    <brandsCampaignStage v-bind:content="true" v-bind:influencer="true" />
+    <div class="bg-vDarkBlue text-white flex items-center mt-5 flex-col gap-5 p-16">
+      <div class="flex w-full gap-5">
+        <div class="basis-1/2 flex flex-col">
+          <p class="text-nowrap">Content Approval Due Date</p>
 
-            <div class="flex w-full gap-5">
-                <div class="basis-1/2 flex flex-col">
-                    <p  >Start Date</p>
+          <UPopover :popper="{ placement: 'bottom-start' }">
+            <UButton
+              class="w-full p-3 border-2 border-darkBlue"
+              :label="format(submissionDueDate, 'd MMM, yyy')"
+              icon="i-heroicons-calendar-days-20-solid"
+            />
 
-                        <UPopover  :popper="{ placement: 'bottom-start' }">
-                            <UButton class="w-full  p-3 border-2  border-darkBlue" icon="i-heroicons-calendar-days-20-solid" :label="format(startDate, 'd MMM, yyy')" />
+            <template #panel="{ close }">
+              <DatePicker v-model="submissionDueDate" is-required @close="close" />
+            </template>
+          </UPopover>
+        </div>
 
-                            <template #panel="{ close }">
-                                <DatePicker v-model="startDate" is-required @close="close" />
-                            </template>
-                        </UPopover>
+        <div class="basis-1/2 flex flex-col">
+          <p>Campaign Start Date</p>
 
-                </div>
+          <UPopover :popper="{ placement: 'bottom-start' }">
+            <UButton
+              class="w-full p-3 border-2 border-darkBlue"
+              icon="i-heroicons-calendar-days-20-solid"
+              :label="format(startDate, 'd MMM, yyy')"
+            />
 
-                <div class="basis-1/2 flex flex-col">
-                    <p  >End Date</p>
+            <template #panel="{ close }">
+              <DatePicker v-model="startDate" is-required @close="close" />
+            </template>
+          </UPopover>
+        </div>
 
-                        <UPopover  :popper="{ placement: 'bottom-start' }">
-                            <UButton class="w-full p-3 border-2 border-darkBlue"
-                            :label="format(endDate, 'd MMM, yyy')"
-                            icon="i-heroicons-calendar-days-20-solid"
-                            />
+        <div class="basis-1/2 flex flex-col">
+          <p>Campaign End Date</p>
 
-                            <template #panel="{ close }">
-                                <DatePicker v-model="endDate" is-required @close="close" />
-                            </template>
-                        </UPopover>
+          <UPopover :popper="{ placement: 'bottom-start' }">
+            <UButton
+              class="w-full p-3 border-2 border-darkBlue"
+              :label="format(endDate, 'd MMM, yyy')"
+              icon="i-heroicons-calendar-days-20-solid"
+            />
 
-                </div>
+            <template #panel="{ close }">
+              <DatePicker v-model="endDate" is-required @close="close" />
+            </template>
+          </UPopover>
+        </div>
+      </div>
 
-            </div>
-
-            <div class=" flex w-full flex-col">
-                    <p>Content Approval Due Date</p>
-
-                        <UPopover  :popper="{ placement: 'bottom-start' }">
-                            <UButton class="w-full p-3 border-2 border-darkBlue"
-                            :label="format(submissionDueDate, 'd MMM, yyy')"
-                            icon="i-heroicons-calendar-days-20-solid"
-                            />
-
-                            <template #panel="{ close }">
-                                <DatePicker v-model="submissionDueDate" is-required @close="close" />
-                            </template>
-                        </UPopover>
-
-                </div>
-
-            <div class="flex w-full gap-5">
-                
-                <!-- <div class="flex basis-1/2 flex-col">
+      <div class="flex w-full gap-5">
+        <!-- <div class="flex basis-1/2 flex-col">
                     <p>Number of posts per influencer</p>
                     <input class="border-2 p-2 bg-transparent border-darkBlue" placeholder="1" type="number" >
                 </div> -->
-            </div>
-
-        </div>
-
-        <div class="flex gap-2 pt-4">
-
-
-                <nuxt-link class="basis-1/3 text-white border-purplebg border-[0.5px] rounded flex items-center justify-center" to="/brands/dashboard/campaigns/create-campaign/campaign-influencer">
-                    <div>
-                        Back
-                    </div>
-                </nuxt-link>
-
-
-            <nuxt-link to="/brands/dashboard/campaigns/create-campaign/campaign-review" class="basis-2/3  text-white bg-[#5331E8] rounded p-3 flex justify-center items-center">
-                <div>
-                    Next
-                </div>
-            </nuxt-link>
-
-        </div>
+      </div>
     </div>
+
+    <div class="flex gap-2 pt-4">
+      <nuxt-link
+        class="basis-1/3 text-white border-purplebg border-[0.5px] rounded flex items-center justify-center"
+        to="/brands/dashboard/campaigns/create-campaign/campaign-influencer"
+      >
+        <div>Back</div>
+      </nuxt-link>
+
+      <nuxt-link
+        to="/brands/dashboard/campaigns/create-campaign/campaign-review"
+        class="basis-2/3 text-white bg-[#5331E8] rounded p-3 flex justify-center items-center"
+      >
+        <div>Next</div>
+      </nuxt-link>
+    </div>
+  </div>
 </template>
