72d4be59a50018b8a6ac63ef14fa316526da0117
eriitunu-a <eritunu.adesioye@useaktivate.com>
2024-07-11 19:12:10 +0100

    startmobile

--- pages/creator/login.vue
+++ pages/creator/login.vue
@@ -3,6 +3,7 @@
 import type { IUser, LoginResponse, ResponseMessage } from "types";
 definePageMeta({
   colorMode: "light",
+  
 });
 
 import UserRoles from "../../enums/userRoles";
@@ -64,62 +65,64 @@ const submitLogin = async (e: Event) => {
 </script>
 
 <template>
-  <div class="basis-1/3">
-    <nuxt-link to="/creator">
+  <div>
+    <div class="basis-1/3">
+      <nuxt-link to="/creator">
       <div class="p-4">
         <signBlackButton message="Sign Up" />
       </div>
-    </nuxt-link>
+      </nuxt-link>
 
-    <div class="px-4 md:px-16 mb-24 flex flex-col gap-6">
-      <h2 class="text-3xl font-semibold">Login to your Account</h2>
-      <p class="text-[#6D6B76]">Login to your Aktivate Creator account</p>
+      <div class="px-4 md:px-16 mb-24 flex flex-col gap-6">
+        <h2 class="text-3xl font-semibold">Login to your Account</h2>
+        <p class="text-[#6D6B76]">Login to your Aktivate Creator account</p>
+      </div>
     </div>
-  </div>
 
-  <div class="flex flex-col justify-center gap-2 basis-2/3">
-    <div>
-      <div class="flex flex-col items-center md:flex-row gap-4 w-full px-4 md:px-16">
-        <div class="flex flex-col w-full md:w-1/2">
-          <label for="">Email </label>
-          <input
-            v-model="email"
-            type="email"
-            placeholder="Your Email Address"
-            class="border rounded border-black py-3 px-2"
-          />
-        </div>
-        <div class="flex flex-col w-full md:w-1/2">
-          <label for="">Password </label>
-          <div
-            class="flex justify-between items-center border p-3 border-1 border-black rounded-md"
-          >
+    <div class="flex flex-col justify-center gap-2 basis-2/3">
+      <div>
+        <div class="flex flex-col items-center md:flex-row gap-4 w-full px-4 md:px-16">
+          <div class="flex flex-col w-full md:w-1/2">
+            <label for="">Email </label>
             <input
-              :type="inputType"
-              class="w-full outline-none pl-2"
-              v-model="password"
-              :placeholder="`enter password`"
-              @keyup.enter="submitLogin"
+              v-model="email"
+              type="email"
+              placeholder="Your Email Address"
+              class="border rounded border-black py-3 px-2"
             />
-            <button type="button" @click="toggleVisibility">
-              {{ showPassword ? "" : "" }} <img src="../../assets/icons/eye.svg" alt="" />
-            </button>
+          </div>
+          <div class="flex flex-col w-full md:w-1/2">
+            <label for="">Password </label>
+            <div
+              class="flex justify-between items-center border p-3 border-1 border-black rounded-md"
+            >
+              <input
+                :type="inputType"
+                class="w-full outline-none pl-2"
+                v-model="password"
+                :placeholder="`enter password`"
+                @keyup.enter="submitLogin"
+              />
+              <button type="button" @click="toggleVisibility">
+                {{ showPassword ? "" : "" }} <img src="../../assets/icons/eye.svg" alt="" />
+              </button>
+            </div>
           </div>
         </div>
+        <div class="flex justify-end px-4 md:px-16">
+          <nuxt-link to="/creator/forgot-password">
+            <button class="text-[#6D6B76]">Forgot Password?</button>
+          </nuxt-link>
+        </div>
       </div>
-      <div class="flex justify-end px-4 md:px-16">
-        <nuxt-link to="/creator/forgot-password">
-          <button class="text-[#6D6B76]">Forgot Password?</button>
-        </nuxt-link>
+
+      <!-- <nuxt-link class="pb-5 md:pb-0" to="/dashboard">
+                  <authButton message="Go To Dashboard "/>
+              </nuxt-link> -->
+
+      <div @click="submitLogin" class="pb-5 md:pb-0">
+        <authButton @click="submitLogin" message="Go To Dashboard" :loading="loading" />
       </div>
-    </div>
-
-    <!-- <nuxt-link class="pb-5 md:pb-0" to="/dashboard">
-                <authButton message="Go To Dashboard "/>
-            </nuxt-link> -->
-
-    <div @click="submitLogin" class="pb-5 md:pb-0">
-      <authButton @click="submitLogin" message="Go To Dashboard" :loading="loading" />
     </div>
   </div>
 </template>
