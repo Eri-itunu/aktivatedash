f924516981cb0881bbdac4bfe9f552cd665a8ea8
eriitunu-a <eritunu.adesioye@useaktivate.com>
2024-07-24 12:34:22 +0100

    mobile pt.3

--- components/LoadSpinner.vue
+++ components/LoadSpinner.vue
@@ -22,10 +22,10 @@
   font-size: 10px;
   position: relative;
   text-indent: -9999em;
-  border-top: 1.1em solid rgba(255, 255, 255, 0.2);
-  border-right: 1.1em solid rgba(255, 255, 255, 0.2);
-  border-bottom: 1.1em solid rgba(255, 255, 255, 0.2);
-  border-left: 1.1em solid #ffffff;
+  border-top: 0.5em solid #5331E8 ;
+  border-right: 0.5em solid #5331E8 ;
+  border-bottom: 0.5em solid #5331E8;
+  border-left: 0.5em solid #ffffff;
   -webkit-transform: translateZ(0);
   -ms-transform: translateZ(0);
   transform: translateZ(0);
--- pages/creator/dashboard/campaigns/[campaignId].vue
+++ pages/creator/dashboard/campaigns/[campaignId].vue
@@ -4,18 +4,18 @@ import {
   getCampaign,
   getSingleCampaignRequest,
 } from "../../../../api/creator/campaign/campaign.creator";
-
+import { ChevronLeft } from 'lucide-vue-next';
 definePageMeta({
   layout: "dashboard",
-  colorMode: "dark",
+
 });
-const showToast = ref(false);
-const { toast } = useToast();
+
+const toast = useToast();
 const userStore = useUserStore();
 const API_URL = useRuntimeConfig().public.API_URL;
 const route = useRoute();
 const router = useRouter();
-
+const showSpinner = ref(false)
 const campaign = ref<ICampaign>();
 const requests = ref<ICampaignRequest[]>([]);
 const loading = ref(true);
@@ -24,7 +24,7 @@ const { campaignId } = route.params;
 const singleCampaignReqs = async () => {
   const { campaignId } = route.params;
   const accessToken = userStore.accessToken || "";
-
+  showSpinner.value = true
   try {
     const platform = await getSingleCampaignRequest({
       apiUrl: API_URL,
@@ -32,11 +32,11 @@ const singleCampaignReqs = async () => {
       accessToken,
     });
     requests.value = platform;
-
+    showSpinner.value = false
     loading.value = false;
   } catch (error: any) {
     loading.value = true;
-
+    
     toast({ title: error.data?.message || "Something went wrong" });
   }
 };
@@ -73,7 +73,8 @@ onMounted(async () => await loadCampaign());
 </script>
 
 <template>
-  <nuxt-link class="mb-2 flex" to="/creator/dashboard/campaigns">
+  <div class=" hidden md:block">
+    <nuxt-link class="mb-2 flex" to="/creator/dashboard/campaigns">
     <svg
       width="24"
       height="24"
@@ -87,180 +88,258 @@ onMounted(async () => await loadCampaign());
       />
     </svg>
     <p>Back</p>
-  </nuxt-link>
+    </nuxt-link>
 
-  <div class="flex flex-col md:flex-row gap-2">
-    <div class="flex px-2 md:px-8 basis-2/3 flex-col gap-2 md:gap-4">
-      <div
-        class="flex px-12 bg-vDarkBlue h-full py-12 rounded-lg flex-col md:flex-row gap-5"
-      >
-        <div class="flex flex-col gap-5 text-white w-full">
-          <div class="flex justify-between border-b-2 py-3 border-darkBlue">
-            <div>
-              <p class="text-purplelabel text-xs">Headline</p>
-              <h4 class="text-3xl text-purplelabel">{{ campaign?.headline }}</h4>
-            </div>
-          </div>
-
-          <!-- <div class="h-[200px]">
-            <img
-              src="/assets/images/pexels-ethan-sees-2741674 2.svg"
-              class="w-full h-full object-cover rounded-lg"
-              alt=""
-            />
-          </div> -->
-
-          <div class="flex relative justify-center bg-purplelabel rounded-lg h-44">
-            <img
-              v-if="campaign?.image"
-              :src="campaign?.image"
-              class="object-fill w-full h-full rounded-lg"
-              alt=""
-            />
-            <img v-else src="/assets/images/created.svg" class="object-fit" alt="" />
-          </div>
-
-          <button v-if="campaign?.brief" @click="openBrief(campaign?.brief)">
-            Open Brief
-          </button>
-
-          <p class="text-wrap">
-            {{ campaign?.description }}
-          </p>
-
-          <div class="flex gap-1 items-center">
-            <!-- icon type thing -->
-            <div class="flex flex-col items-center max-w-min">
-              <div class="h-2 w-2 rounded-full bg-grey1"></div>
-              <div class="h-4 w-[0.05rem] rounded-full bg-grey1"></div>
-              <div class="h-2 w-2 rounded-full bg-grey1"></div>
-            </div>
-            <!-- end icon thing-->
-            <div class="text-sm text-[#CDC2FF] text-nowrap">
-              <p>
-                Start Date:
-                <span class="font-light text-xs">{{
-                  campaign?.start_date?.split("T")[0]
-                }}</span>
-              </p>
-              <p>
-                End Date:
-                <span class="font-light text-xs">{{
-                  campaign?.end_date?.split("T")[0]
-                }}</span>
-              </p>
-            </div>
-          </div>
-
-          <div class="flex gap-5">
-            <div class="flex flex-col gap-1">
-              <p class="text-purplelabel capitalize">Content Type</p>
-              <p
-                class="capitalize"
-                v-for="ctnType in campaign?.deliverables?.content_type"
-                :key="ctnType"
-              >
-                {{ ctnType }}
-              </p>
-            </div>
-
-            <div class="flex flex-col gap-1">
-              <p class="text-purplelabel">Platform Type</p>
-              <div class="flex gap-1 overflow-hidden">
-                <img
-                  v-if="campaign?.deliverables?.platform.includes('instagram')"
-                  class="object-contain"
-                  src="/assets/icons/collab/instagram.svg"
-                  alt=""
-                />
-                <img
-                  v-if="campaign?.deliverables?.platform.includes('linkedin')"
-                  class="object-contain"
-                  src="/assets/icons/collab/linkedin.svg"
-                  alt=""
-                />
-                <img
-                  v-if="campaign?.deliverables?.platform.includes('facebook')"
-                  class="object-contain"
-                  src="/assets/icons/collab/facebook.svg"
-                  alt=""
-                />
-                <img
-                  v-if="campaign?.deliverables?.platform.includes('tiktok')"
-                  class="object-contain"
-                  src="/assets/icons/collab/tiktok.svg"
-                  alt=""
-                />
-                <img
-                  v-if="campaign?.deliverables?.platform.includes('x')"
-                  class="object-contain"
-                  src="/assets/icons/collab/twitter.svg"
-                  alt=""
-                />
-                <img
-                  v-if="campaign?.deliverables?.platform.includes('whatsapp')"
-                  class="object-contain"
-                  src="/assets/icons/collab/whatsapp.svg"
-                  alt=""
-                />
-                <img
-                  v-if="campaign?.deliverables?.platform.includes('snapchat')"
-                  class="object-contain"
-                  src="/assets/icons/collab/snapchat.svg"
-                  alt=""
-                />
-                <img
-                  v-if="campaign?.deliverables?.platform.includes('youtube')"
-                  class="object-contain"
-                  src="/assets/icons/collab/youtube.svg"
-                  alt=""
-                />
+    <div class="flex flex-col md:flex-row gap-2">
+      <div class="flex px-2 md:px-8 basis-2/3 flex-col gap-2 md:gap-4">
+        <div
+          class="flex px-12 bg-vDarkBlue h-full py-12 rounded-lg flex-col md:flex-row gap-5"
+        >
+          <div class="flex flex-col gap-5 text-white w-full">
+            <div class="flex justify-between border-b-2 py-3 border-darkBlue">
+              <div>
+                <p class="text-purplelabel text-xs">Headline</p>
+                <h4 class="text-3xl text-purplelabel">{{ campaign?.headline }}</h4>
               </div>
             </div>
-          </div>
 
-          <div>
-            <h4>Requirements</h4>
-            <li>{{ campaign?.deliverables?.requirements }}</li>
+            <!-- <div class="h-[200px]">
+              <img
+                src="/assets/images/pexels-ethan-sees-2741674 2.svg"
+                class="w-full h-full object-cover rounded-lg"
+                alt=""
+              />
+            </div> -->
+
+            <div class="flex relative justify-center bg-purplelabel rounded-lg h-44">
+              <img
+                v-if="campaign?.image"
+                :src="campaign?.image"
+                class="object-fill w-full h-full rounded-lg"
+                alt=""
+              />
+              <img v-else src="/assets/images/created.svg" class="object-fit" alt="" />
+            </div>
+
+            <button v-if="campaign?.brief" @click="openBrief(campaign?.brief)">
+              Open Brief
+            </button>
+
+            <p class="text-wrap">
+              {{ campaign?.description }}
+            </p>
+
+            <div class="flex gap-1 items-center">
+              <!-- icon type thing -->
+              <div class="flex flex-col items-center max-w-min">
+                <div class="h-2 w-2 rounded-full bg-grey1"></div>
+                <div class="h-4 w-[0.05rem] rounded-full bg-grey1"></div>
+                <div class="h-2 w-2 rounded-full bg-grey1"></div>
+              </div>
+              <!-- end icon thing-->
+              <div class="text-sm text-[#CDC2FF] text-nowrap">
+                <p>
+                  Start Date:
+                  <span class="font-light text-xs">{{
+                    campaign?.start_date?.split("T")[0]
+                  }}</span>
+                </p>
+                <p>
+                  End Date:
+                  <span class="font-light text-xs">{{
+                    campaign?.end_date?.split("T")[0]
+                  }}</span>
+                </p>
+              </div>
+            </div>
+
+            <div class="flex gap-5">
+              <div class="flex flex-col gap-1">
+                <p class="text-purplelabel capitalize">Content Type</p>
+                <p
+                  class="capitalize"
+                  v-for="ctnType in campaign?.deliverables?.content_type"
+                  :key="ctnType"
+                >
+                  {{ ctnType }}
+                </p>
+              </div>
+
+              <div class="flex flex-col gap-1">
+                <p class="text-purplelabel">Platform Type</p>
+                <div class="flex gap-1 overflow-hidden">
+                  <img
+                    v-if="campaign?.deliverables?.platform.includes('instagram')"
+                    class="object-contain"
+                    src="/assets/icons/collab/instagram.svg"
+                    alt=""
+                  />
+                  <img
+                    v-if="campaign?.deliverables?.platform.includes('linkedin')"
+                    class="object-contain"
+                    src="/assets/icons/collab/linkedin.svg"
+                    alt=""
+                  />
+                  <img
+                    v-if="campaign?.deliverables?.platform.includes('facebook')"
+                    class="object-contain"
+                    src="/assets/icons/collab/facebook.svg"
+                    alt=""
+                  />
+                  <img
+                    v-if="campaign?.deliverables?.platform.includes('tiktok')"
+                    class="object-contain"
+                    src="/assets/icons/collab/tiktok.svg"
+                    alt=""
+                  />
+                  <img
+                    v-if="campaign?.deliverables?.platform.includes('x')"
+                    class="object-contain"
+                    src="/assets/icons/collab/twitter.svg"
+                    alt=""
+                  />
+                  <img
+                    v-if="campaign?.deliverables?.platform.includes('whatsapp')"
+                    class="object-contain"
+                    src="/assets/icons/collab/whatsapp.svg"
+                    alt=""
+                  />
+                  <img
+                    v-if="campaign?.deliverables?.platform.includes('snapchat')"
+                    class="object-contain"
+                    src="/assets/icons/collab/snapchat.svg"
+                    alt=""
+                  />
+                  <img
+                    v-if="campaign?.deliverables?.platform.includes('youtube')"
+                    class="object-contain"
+                    src="/assets/icons/collab/youtube.svg"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <h4>Requirements</h4>
+              <li>{{ campaign?.deliverables?.requirements }}</li>
+            </div>
           </div>
+        </div>
+
+        <div class="flex gap-2 px-2 md:px-0 md:pt-4">
+          <!-- <button class="basis-1/3 text-white border-purplebg border-[0.5px] rounded">
+            <nuxt-link to="/brands/dashboard/campaigns/"> Back </nuxt-link>
+          </button> -->
         </div>
       </div>
 
-      <div class="flex gap-2 px-2 md:px-0 md:pt-4">
-        <!-- <button class="basis-1/3 text-white border-purplebg border-[0.5px] rounded">
-          <nuxt-link to="/brands/dashboard/campaigns/"> Back </nuxt-link>
-        </button> -->
-      </div>
-    </div>
+      <div class="basis-1/3 flex flex-col gap-2">
+        <div class="flex flex-col gap-4" v-if="loading">
+          <CreatorLoadingCard />
+          <CreatorLoadingCard />
+          <CreatorLoadingCard />
+        </div>
 
-    <div class="basis-1/3 flex flex-col gap-2">
-      <div class="flex flex-col gap-4" v-if="loading">
-        <CreatorLoadingCard />
-        <CreatorLoadingCard />
-        <CreatorLoadingCard />
-      </div>
-
-      <div v-else v-for="request in requests" :key="request.id">
-        <CreatorCampaignRequestCard :request="request" :ID="campaignId" />
+        <div v-else v-for="request in requests" :key="request.id">
+          <CreatorCampaignRequestCard :request="request" :ID="campaignId" />
+        </div>
       </div>
     </div>
   </div>
-  <transition name="toast">
-    <Toast v-if="showToast" error="Error mehn" />
-  </transition>
+
+  <div v-if="showSpinner"  class=" md:hidden w-[100%] h-[100%] fixed top-0 right-0 left-0 bottom-0 z-50 bg-[#000000]/ flex justify-center items-center">
+    <LoadSpinner />
+  </div>
+
+  <div v-else class="md:hidden bg-white text-black px-4 py-4">
+    <div class="w-full">
+      <img src="/assets/icons/CampaignMain.svg" class="w-full " alt="">
+      <ChevronLeft @click="router.back()" class=""/>
+    </div>
+
+    <!-- headline and date section -->
+    <section class="border-b-[0.5px] border-grey-border py-4 flex flex-col gap-4" >
+      <h1 class="font-bold" >{{ campaign?.headline }}</h1>
+      <div class="flex justify-between" >
+        <div>
+          <h2 class="text-grey-text">START DATE</h2> 
+          <p>{{ campaign?.start_date?.split("T")[0]}}</p>
+        </div>
+        <div>
+          <h2 class="text-grey-text">END DATE</h2>
+          <p>{{  campaign?.end_date?.split("T")[0] }}</p>
+        </div>
+      </div>
+    </section>
+
+    <!-- content type section -->
+    <section class="border-b-[0.5px] border-grey-border py-4">
+      <h2 class="text-grey-text">CONTENT TYPE</h2>
+      <div class='flex  gap-2 pt-2' >
+        <div
+          class="text-purple1 rounded-lg py-1 px-2 bg-purplelabel w-fit"
+          v-for="ctnType in campaign?.deliverables?.content_type"
+          :key="ctnType"
+        >
+          {{ ctnType }}
+        </div>
+      </div>
+    </section>
+
+    <!-- platform  section -->
+
+    <section class="border-b-[0.5px] border-grey-border py-4">
+      <h2 class="text-grey-text" >PLATFORM </h2>
+      <div class="flex gap-1 overflow-hidden">
+        <img
+          v-if="campaign?.deliverables?.platform.includes('instagram')"
+          class="object-contain"
+          src="/assets/icons/collab/instagramWhite.svg"
+          alt=""
+        />
+       
+        <!-- <img
+          v-if="campaign?.deliverables?.platform.includes('facebook')"
+          class="object-contain"
+          src="/assets/icons/collab/facebook.svg"
+          alt=""
+        /> -->
+        <img
+          v-if="campaign?.deliverables?.platform.includes('tiktok')"
+          class="object-contain"
+          src="/assets/icons/collab/tiktokWhite.svg"
+          alt=""
+        />
+        <img
+          v-if="campaign?.deliverables?.platform.includes('x')"
+          class="object-contain"
+           src="/assets/icons/collab/xWhite.svg"
+          alt=""
+        />
+       
+      </div>
+    </section>
+    
+    <!-- Campaign requirements section -->
+
+    <section class="border-b-[0.5px] border-grey-border py-4 text-grey-text">
+      <h2>REQUIREMENTS </h2>
+      {{ campaign?.deliverables?.requirements }}
+    </section>
+
+    <!-- Campaign decision section -->
+
+    <section class="border-b-[0.5px] border-grey-border py-4 ">
+      <h2>Campaign Price </h2>
+      <div class="flex flex-col pt-4 gap-4">
+        <div  v-for="request in requests" :key="request.id">
+          <CreatorDecisionCard :request="request" :ID="campaignId" />
+        </div>
+      </div>
+    </section>
+
+
+  </div>
 </template>
-
-<style>
-.toast-enter-from {
-  opacity: 0;
-  transform: translateX(-60px);
-}
-.toast-enter-to {
-  opacity: 1;
-  transform: translateX(0);
-}
-
-.toast-enter-active {
-  transition: all 0.3s ease;
-}
-</style>
+>
