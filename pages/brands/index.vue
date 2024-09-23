7ec0ddf2ef79a40eba1c03fdc725b0616f1b9ef8
eriitunu-a <adesioyeeriitunu@gmail.com>
2024-09-23 13:33:49 +0100

    Update index.vue

--- pages/brands/index.vue
+++ pages/brands/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import UserRoles from "../../enums/userRoles";
+import UserRoles from "@/enums/userRoles";
 import { useToast } from "../../components/ui/toast";
 
 definePageMeta({
