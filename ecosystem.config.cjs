0dbc5233d1ae862ae1e4e5b845ccd3355b6a2bed
eiromz <eronmonsele.adeyemo@gmail.com>
2024-11-10 22:33:47 +0100

    added ecosystem config for dev, staging and demo

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,13 +1,14 @@
 module.exports = {
-    apps : [{
-        name: 'app.useaktivate.com',
-        script:'.output/server/index.mjs',
-        out_file: "out.log",
-        error_file: "error.log",
-        merge_logs: true,
-        log_date_format: "DD-MM HH:mm:ss Z",
-        log_type: "json",
-    },
+    apps : [
+        {
+            name: 'app.useaktivate.com',
+            script:'.output/server/index.mjs',
+            out_file: "out.log",
+            error_file: "error.log",
+            merge_logs: true,
+            log_date_format: "DD-MM HH:mm:ss Z",
+            log_type: "json",
+        },
         {
             name: 'demo-app.useaktivate.com',
             script:'.output/server/index.mjs',
