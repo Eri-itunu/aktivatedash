d587a854467b98c39f9ef8780198c1ccc7c0c484
eiromz <eronmonsele.adeyemo@gmail.com>
2024-11-11 10:27:46 +0100

    working on pm2 config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -22,7 +22,7 @@ module.exports = {
             log_type: "json",
         },
         {
-            name: 'staging-app.useaktivate.com',
+            name: 'dev-app.useaktivate.com',
             script:'.output/server/index.mjs',
             out_file: "out.log",
             error_file: "error.log",
