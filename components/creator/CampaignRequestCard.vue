2e6a03f57e69a4fbbfa1fcacbcb6aac07dacda73
eriitunu-a <adesioyeeriitunu@gmail.com>
2024-12-27 14:38:48 +0100

    file upload progress bar

--- components/creator/CampaignRequestCard.vue
+++ components/creator/CampaignRequestCard.vue
@@ -188,7 +188,7 @@ const linkPost = async (platformProfileId: string | undefined, contentId: string
           v-if="decisionState === 'accept'"
           class="rounded-full cursor-pointer text-center px-2  bg-purple1 h-fit py-1"
         >
-          Upload Content for Approval
+          Link Posts 
         </button>
       </div>
       <div v-if="decisionState === 'pending'" class="flex gap-2">
