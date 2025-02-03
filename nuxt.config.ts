f22c301df6bfe3d5db6e15137bb34919d680c500
eiromz <eronmonsele.adeyemo@gmail.com>
2025-02-03 10:39:53 +0100

    added allowed host to nuxtconfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
+     vite: {
+          server: {
+            allowedHosts: ['dev-app.useaktivate.com','staging-app.useaktivate.com','app.useaktivate.com']
+          }
+        },
   app: {
       head: {
           title: 'Aktivate Influencer Marketing ',
