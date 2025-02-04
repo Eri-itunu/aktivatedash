2b0c5eff4e943fedde1a311f7f9c229ab5dcdb70
eiromz <eronmonsele.adeyemo@gmail.com>
2025-02-04 10:22:44 +0100

    modified pm2 config file for development

--- ecosystem.dev.config.cjs
+++ ecosystem.dev.config.cjs
@@ -1,15 +1,23 @@
 module.exports = {
      apps : [
          {
-             name: 'dev-app.useaktivate.com',
-             script: 'yarn',
-             args: 'dev',
-             port:'8080',
-             out_file: "out.log",
-             error_file: "error.log",
-             merge_logs: true,
-             log_date_format: "DD-MM HH:mm:ss Z",
-             log_type: "json",
-         }
+          name: 'dev-app.useaktivate.com',
+          script: "node_modules/nuxt/bin/nuxt",
+          args: "dev",
+          port:'8080',
+          out_file: "out.log",
+          error_file: "error.log",
+          merge_logs: true,
+          log_date_format: "DD-MM HH:mm:ss Z",
+          log_type: "json",
+          env: {
+
+            NODE_ENV: "development"
+
+        },
+
+        watch: true  // Optionally enable file watching for automatic reloads
+
+      }
      ]
  };
