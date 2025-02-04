4b5aacb94bba5d7df2c84e014e2b98d8380b2904
eiromz <eronmonsele.adeyemo@gmail.com>
2025-02-04 14:35:45 +0100

    changed pm2 deploy document

--- ecosystem.dev.config.cjs
+++ ecosystem.dev.config.cjs
@@ -2,22 +2,13 @@ module.exports = {
      apps : [
          {
           name: 'dev-app.useaktivate.com',
-          script: "node_modules/nuxt/bin/nuxt",
-          args: "dev",
+          script:'.output/server/index.mjs',
           port:'8080',
           out_file: "out.log",
           error_file: "error.log",
           merge_logs: true,
           log_date_format: "DD-MM HH:mm:ss Z",
           log_type: "json",
-          env: {
-
-            NODE_ENV: "development"
-
-        },
-
-        watch: true  // Optionally enable file watching for automatic reloads
-
       }
      ]
  };
