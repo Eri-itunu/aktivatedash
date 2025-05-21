63aab3f976d54f7bbafd1c98c7afab1806f545da
eriitunu-a <adesioyeeriitunu@gmail.com>
2025-05-21 13:56:07 +0100

    creator campaign image fix

--- components/creator/CampaignCard.vue
+++ components/creator/CampaignCard.vue
@@ -61,7 +61,15 @@ const socialPlatforms = computed(() => [
       <!-- Campaign Image -->
       <div class="flex relative justify-center bg-purplelabel rounded-lg h-40">
         <img
-          :src="campaign.images?.[0] || '/assets/images/created.svg'"
+          v-if="campaign.images?.[0] != null"
+          :src="campaign.images?.[0] "
+          class="object-fit w-full h-full"
+          :class="{ 'w-full h-full': campaign.images?.[0] }"
+          alt="Campaign Image"
+        />
+        <img
+          v-else
+          src="/assets/images/created.svg"
           class="object-fit w-full h-full"
           :class="{ 'w-full h-full': campaign.images?.[0] }"
           alt="Campaign Image"
