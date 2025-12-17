1b3d57446e160dbbdffb898705b13a20328236c0
Oluwaseyi (Ekpere) Oyewunmi <thoth2357@gmail.com>
2025-12-17 18:19:56 +0100

    👷 Changed port of app

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,16 +1,20 @@
 module.exports = {
-    apps : [
-        {
-            name: 'app.useaktivate.com',
-            script:'.output/server/index.mjs',
-            out_file: "out.log",
-            error_file: "error.log",
-            merge_logs: true,
-            log_date_format: "DD-MM HH:mm:ss Z",
-            log_type: "json",
-            node_args: '--no-deprecation',
-            instances: "3",
-            exec_mode: "cluster",
-        }
-    ]
+	apps: [
+		{
+			name: "app.useaktivate.com",
+			script: ".output/server/index.mjs",
+			out_file: "out.log",
+			error_file: "error.log",
+			merge_logs: true,
+			log_date_format: "DD-MM HH:mm:ss Z",
+			log_type: "json",
+			node_args: "--no-deprecation",
+			instances: "3",
+			exec_mode: "cluster",
+			env: {
+				PORT: 3001,
+				NODE_ENV: "production",
+			},
+		},
+	],
 };
